1773aada58399cd5a14d9118c0c80db1d68c3908
allanford17 <allan.ford17@gmail.com>
2024-02-02 14:41:58 +1030

    updated Feb 2024

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World version 2  !");
+
+double y;
+y = 1.234567;
+
+y = 1.2;
+
+//string abc;
+
+//abc := y.ToString("0.##");
+
+Console.WriteLine(y.ToString("0.##"));
+Console.WriteLine(y.ToString("0.00"));
+Console.WriteLine(y.ToString("0.000"));
+Console.WriteLine(y.ToString("0.0#"));
+Console.WriteLine(y.ToString("0.###" ));
